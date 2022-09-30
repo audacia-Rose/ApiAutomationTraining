d462a6b8f572507d924a889887bb966aa1b9d16b
audacia-Rose <rose.hadley@audacia.co.uk>
2022-09-30 15:58:17 +0100

    Updated Put test to include base customisations and data setup/cleanup

--- APITestingTemplate/APITestingTemplate/Tests/Academy/PutBookTests (2).cs
+++ APITestingTemplate/APITestingTemplate/Tests/Academy/PutBookTests (2).cs
@@ -32,33 +32,23 @@ namespace APITestingTemplate.Tests.Academy
         public void Scenario_1_As_a_user_I_can_update_a_book()
         {
             // Create a new book category and book to update using the fixtures
-
-            // Create new book category & capture its ID
             var bookCategoryData = _addBookAndCategoryFixture.BookData.BookCategoryData;
             var bookCategoryId = bookCategoryData.First().Id;
             var bookData = _addBookAndCategoryFixture.BookData.BookData;
             var bookId = bookData.First().Id;
 
-            // Set up the request body for a new book using our base customisation
-            var addBookRequest = SetupWithoutSave<AddBookRequest>();
-
-            // Base customisation sets book category to 1 so we should override this
-            addBookRequest.BookCategoryId = bookCategoryId;
-
-
-
-
             // Setting up the request body for updating a book
             var updateBookRequest = SetupWithoutSave<UpdateBookRequest>();
 
-            updateBookRequest.Id = 1500;
-            updateBookRequest.Title = "All My Friends Are Alive";
+            updateBookRequest.Id = bookId;
+            updateBookRequest.Title = "All My Friends Are Dead";
             updateBookRequest.Description = "If you're a dinosaur, all of your friends are dead. If you're a pirate, all of your friends have scurvy. If you're a tree, all of your friends are end tables.";
-            updateBookRequest.Author = "Avery Monsen, Jory John, Rose Hadley";
+            updateBookRequest.Author = "Avery Monsen, Jory John";
             updateBookRequest.PublishedYear = 2022;
             updateBookRequest.AvailableFrom = DateTimeOffset.Now;
             updateBookRequest.BookCategoryId = 13;
             updateBookRequest.HasEBook = true;
+            // *hello*
 
             // Call the get API to update given book with above details
             var updateBookResponse = Put<UpdateBookRequest>(updateBookRequest, Resources.UpdateBook);
